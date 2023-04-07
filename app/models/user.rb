4e4f89a9a2ab10fc55d74a0e1592cf0fa4aee698
dewanggaptr <dewanggajr30@gmail.com>
2023-04-07 13:31:06 +0700

    add profile features

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  belongs_to :job
+
+  has_one :profile, dependent: :destroy
+
   has_many :user_team
   has_many :team, through: :user_team
 
@@ -16,8 +18,7 @@ class User < ApplicationRecord
   has_many :user_notes
   has_many :notes, through: :user_notes, source: :note
 
-  validates :username, presence: true, length: { maximum: 100 }
-  validates :email, presence: true, length: { maximum: 100 },
+  validates :email, presence: true, length: { maximum: 50 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
   validates :password, length: { minimum: 8 }
   validates :password_requirements, confirmation: true
@@ -26,10 +27,7 @@ class User < ApplicationRecord
   def new_attr
     {
       id:,
-      username:,
-      email:,
-      phone:,
-      job: job.new_attr,
+      email:
     }
   end
 
