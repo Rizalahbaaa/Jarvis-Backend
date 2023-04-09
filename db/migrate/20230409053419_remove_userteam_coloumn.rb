80b372d28a478aa0292fa11009bba77e3728f6a0
Muhammad iqbalk <90741174+MuhammadIqbalK@users.noreply.github.com>
2023-04-09 12:56:07 +0700

    Update 20230409053419_remove_userteam_coloumn.rb
    benerin dikit

--- db/migrate/20230409053419_remove_userteam_coloumn.rb
+++ db/migrate/20230409053419_remove_userteam_coloumn.rb
@@ -1,5 +1,6 @@
 class RemoveUserteamColoumn < ActiveRecord::Migration[7.0]
   def change
-    remove_column :invitation_code, :invitation_status
+    remove_column :user_teams, :invitation_status
+    remove_column :user_teams, :invitation_code
   end
 end
