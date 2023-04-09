e35b1a3f13a3213fcb9034e77ec792cc86537566
hasbizulfan24 <hasbizulfan24@gmail.com>
2023-04-09 14:01:26 +0700

    invitation and notification model, migration and controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,20 @@ Rails.application.routes.draw do
     get '/users', to: 'users#index'
     post '/register', to: 'users#register'
 
+    resources :invitations
+    resources :notivications
+    resources :ringtones
+    resources :team_notes
+    resources :user_team_notes
+    resources :users
+    resources :profiles
     resources :jobs
     resources :teams
     resources :lists
     resources :notes
     resources :users_notes
+    resources :user_teams
+    resources :progress
+    resources :attaches
   end
 end
