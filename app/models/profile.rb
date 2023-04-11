817e12f934267e5adcbc0d032d0d3c540427e5e6
RizBlog111 <Rizalahba31@gmail.com>
2023-04-11 11:07:06 +0700

    update models

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,8 @@ class Profile < ApplicationRecord
   belongs_to :user
   belongs_to :job
   has_many :progress
+  has_many :transactions
+  has_many :product, through: :transaction
   validates :username, presence: true, length: { maximum: 50 }
   validates :job_id, presence: true
   validates :phone, presence: true, length: { minimum: 11 }, uniqueness: true, numericality: true,
