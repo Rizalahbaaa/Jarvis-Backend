31b4d97a40b16a0aa1aeae37811a4a2642599512
dewanggaptr <dewanggajr30@gmail.com>
2023-04-16 14:20:39 +0700

    adding user register feature

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
   has_many :user_notes
-  has_many :profile, through: :user_notes, source: :profile
+  has_many :users, through: :user_notes, source: :user
   has_many :notification
   has_many :invitation, as: :invitetable
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_14_060149) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_16_034307) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_14_060149) do
 
   create_table "teams", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "username"
+    t.string "phone"
+    t.string "job"
+    t.text "photo"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
