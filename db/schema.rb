28bd678a8ea40c69ac34affeefedc3eb2af0c58b
ikanurfitriani <ikan07393@gmail.com>
2023-04-18 12:24:17 +0700

    Column migration

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_044311) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "columns", force: :cascade do |t|
+    t.string "title"
+    t.integer "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "reward"
