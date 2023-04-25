d981d81add3b5af0b845929c1a623f09760ad915
dewanggaptr <dewanggajr30@gmail.com>
2023-04-25 23:38:19 +0700

    fixing  attributes on notes model

--- app/models/note.rb
+++ app/models/note.rb
@@ -4,34 +4,37 @@ class Note < ApplicationRecord
   has_many :notification
   has_many :invitation, as: :invitetable
 
-  has_many :progresses
-
-  has_many :reminders
-
-  belongs_to :list, optional: true
+  belongs_to :column, optional: true
+  belongs_to :ringtone
 
   validates :subject, presence: true, length: { maximum: 30 }
   validates :description, presence: true, length: { maximum: 100 }
   validates :event_date, presence: true
-  validates :ringtone_id, presence: false
-  validates :note_type, presence: true
-  validates :list_id, presence: false
+  validates :ringtone_id, presence: true
+  validates :column_id, presence: false
 
   enum note_type: {
     personal: 0,
-    collaboration: 1
+    collaboration: 1,
+    team: 2
+  }
+
+  enum status: {
+    in_progress: 0,
+    completed: 1
   }
 
   def new_attr
     {
-      id: self.id,
+      id:,
       subject:,
       description:,
       event_date:,
-      reminder_date: reminders.map{ |reminder| reminder.reminder_date },
+      ringtone: ringtone.name,
+      column: self.column&.title,
       note_type:,
-      list: self.list&.title,
-      member: profile.map { |profile| profile.user.email }
+      status:
+      # member: profile.map { |profile| profile.user.email }
     }
   end
 end
