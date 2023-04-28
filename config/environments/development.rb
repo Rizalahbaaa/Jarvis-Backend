abdd97f576a4919010b38fa8d3254c0a2db5a9af
dewanggaptr <dewanggajr30@gmail.com>
2023-04-28 14:53:44 +0700

    fixing email confirm in dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,8 +63,8 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # config.action_mailer.default_url_options = { host: 'bantuin.fly.dev', protocol: 'https' }
   # config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000', protocol: 'http' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
