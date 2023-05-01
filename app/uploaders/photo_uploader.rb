03c323ef907bb9d71f186f1d19638c768b8a0e1d
dewanggaptr <dewanggajr30@gmail.com>
2023-05-02 00:08:40 +0700

    add image processing

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,9 +1,14 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
+  include Cloudinary::CarrierWave
   # include CarrierWave::RMagick
+  # include CarrierWave::Processing::RMagick
   # include CarrierWave::MiniMagick
 
-  include Cloudinary::CarrierWave
+  process resize_to_fill: [500, 500]
+  process convert: 'png'
+  process colorspace: :rgb
+  process quality: 80
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -14,6 +19,14 @@ class PhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_allowlist
+    %w[jpg jpeg png]
+  end
+
+  # def size_range
+  #   102_400...512_000
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
@@ -37,9 +50,6 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_allowlist
-  #   %w(jpg jpeg gif png)
-  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
