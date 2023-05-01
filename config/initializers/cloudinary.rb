de26708015181822234bb086ec136dff703651e4
dewanggaptr <dewanggajr30@gmail.com>
2023-05-02 01:02:59 +0700

    updating cdn  config with secret cdn

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,7 +1,9 @@
+require 'dotenv/load'
+
 Cloudinary.config do |config|
   config.cloud_name = 'dis2k0keq'
   config.api_key = '578344134129924'
-  config.api_secret = '8kEXc4Uf4l38s3kM51xIbOmH708'
+  config.api_secret = ENV['CLOUDINARY_SECRET']
   config.secure = true
   config.cdn_subdomain = true
 end
