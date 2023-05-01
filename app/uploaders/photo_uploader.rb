327bc5454f06869ca3317bf30cb9a55ccac6c253
dewanggaptr <dewanggajr30@gmail.com>
2023-05-02 01:02:21 +0700

    make secure filename in photo_profile dir

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -16,8 +16,20 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  def public_id
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "public/profile_picture/#{filename}"
+  end
+
+  def filename
+    "#{secure_token}" if original_filename.present?
+  end
+
+  protected
+
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 
   def extension_allowlist
