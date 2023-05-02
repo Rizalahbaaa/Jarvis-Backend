e1f9ed22de00e7952d7229b1ce526793236d43b5
RizBlog111 <Rizalahba31@gmail.com>
2023-05-03 06:05:49 +0700

    Update Validates Reward

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ApplicationRecord
     has_many :transactions
     has_many :users, through: :transactions
+    
     validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
     validates :terms, presence: true ,length: { maximum: 100 }
-    validates :reward, presence: true, length: { maximum: 50 }, uniqueness: true
+    validates :reward, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
     validates :price, presence: true, numericality: { greater_than: 0 }
     def new_attr
         {
