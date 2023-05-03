a52b2d3626ed4bd17cd5d2d61116861c90dfe4a9
dewanggaptr <dewanggajr30@gmail.com>
2023-05-03 10:03:41 +0700

    add reset_token and reset_sent_at column on user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_26_042357) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_02_150733) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,5 +93,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_26_042357) do
     t.datetime "updated_at", null: false
     t.boolean "email_confirmed"
     t.string "confirm_token"
+    t.string "password_reset_token"
+    t.datetime "password_reset_sent_at"
   end
+
 end
