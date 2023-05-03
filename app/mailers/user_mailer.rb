2e92d16525bcef5882fb517e2e97d14c6032b008
dewanggaptr <dewanggajr30@gmail.com>
2023-05-03 10:15:54 +0700

    update url on user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,20 +4,20 @@ class UserMailer < ApplicationMailer
 
   def registration_confirmation(user)
     @user = user
-    # @url = ENV['ROOT_URL']
+    @url = ENV['ROOT_URL']
 
     # if you want to use email for development, please uncommment @url below and comment @url above
-    @url = "http://127.0.0.1:3000/api"
+    # @url = "http://127.0.0.1:3000/api"
 
     mail(to: "#{user.username} <#{user.email}>", subject: 'Registration Confirmation')
   end
 
   def forgot_password(user)
     @user = user
-    # @url = ENV['ROOT_URL']
+    @url = ENV['ROOT_URL']
 
     # if you want to use email for development, please uncommment @url below and comment @url above
-    @url = "http://127.0.0.1:3000/api"
+    # @url = "http://127.0.0.1:3000/api"
 
     mail(to: "#{user.username} <#{user.email}>", subject: 'Reset Password Instruction')
   end
